7878d057def4f01bd6c2fc1ca2e84fc156ac0c52
Tamir <tamir.gluzman@post.runi.ac.il>
2023-12-29 16:21:25 +0200

    Fixed printing of extra spaces in DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,10 +6,13 @@ public class DamkaBoard {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (i % 2 == 1 && j == 0) {
-					System.out.print(" ");
+				if (i % 2 == 1) {
+					System.out.print(" *");
 				}
-				System.out.print("* ");
+				else {
+					System.out.print("* ");
+				}
+				
 			}
 			System.out.println();
 		}
